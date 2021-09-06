54ea1c1dc1a9e92b8eadb474af24ceac4867206a
n1xx1 <me@simonemiraglia.it>
2021-09-06 10:36:50 +0200

    fix build permissions

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,13 @@
 const esbuild = require("esbuild");
-const { mkdir, copyFile, rm } = require("fs/promises");
+const { mkdir, copyFile, rm, chmod } = require("fs/promises");
+const { constants } = require("fs");
 
 (async () => {
   await rm("dist", { recursive: true, force: true }).catch(() => {});
-  await mkdir("dist").catch(() => {});
+  await mkdir("dist", 0o777).catch(() => {});
+
   await copyFile("./static/module.json", "./dist/module.json");
+  await chmod("./dist/module.json", 0o777);
 
   const prod = process.argv[process.argv.length - 1] === "production";
   const watch = process.argv[process.argv.length - 1] === "watch";
